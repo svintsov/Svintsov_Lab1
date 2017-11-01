d74c256fcd83c3d11532bf967bd2c9ec1c9b2a64
bazyl <svintcov95@gmail.com>
2017-11-01 12:00:44 +0200

    Javadocs were created

--- src/com/bazyl/credits/Main.java
+++ src/com/bazyl/credits/Main.java
@@ -5,8 +5,20 @@ import com.bazyl.credits.sorters.AbstractSortable;
 import com.bazyl.credits.sorters.DefaultSorter;
 import com.bazyl.credits.view.ConsoleOutput;
 
+/**
+ * Main entry point class of the application. Consist only one method named main
+ *
+ * @author Kyrylo Svintsov
+ * @version %I%
+ * @since 1.5
+ */
 public class Main {
 
+  /**
+   * Main method of the application. Creates arrays and sorts them, shows input to a console.
+   *
+   * @param args default params when starting the app
+   */
   public static void main(String[] args) {
     int[] myArray = ArrayCreator.getRandomArray(10);
     ConsoleOutput.showArray(myArray);
